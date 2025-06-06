86bc7108c145de29c584baebc736e69f1ef61720
Lux <1171484646@qq.com>
2025-06-06 15:42:12 +0800

    user process styles completed

---
+++ components/user-progress.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
+type UserProgressProps = {
+  activeCourse: { imageSrc: string; title: string }; // TODO: Replace with DB types
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+export const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: UserProgressProps) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/course">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            width={32}
+            height={32}
+            className="rounded-md border"
+          />
+        </Button>
+      </Link>
+
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            alt="Points"
+            width={28}
+            height={28}
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            alt="Hearts"
+            width={22}
+            height={22}
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="w-4 h-4 stroke-3" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  );
+};
