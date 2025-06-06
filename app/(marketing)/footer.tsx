1edc71d62b3e27c44ba8266613b37e3cb6b5500c
Lux <1171484646@qq.com>
2025-06-06 11:41:40 +0800

    footer page done

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,51 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <div className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button variant="ghost" size="lg">
+          <Image
+            src="/hr.svg"
+            width={40}
+            height={32}
+            alt="Croatian"
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button variant="ghost" size="lg">
+          <Image
+            src="/es.svg"
+            width={40}
+            height={32}
+            alt="Spanish"
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button variant="ghost" size="lg">
+          <Image
+            src="/fr.svg"
+            width={40}
+            height={32}
+            alt="French"
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button variant="ghost" size="lg">
+          <Image
+            src="/it.svg"
+            width={40}
+            height={32}
+            alt="Italian"
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+      </div>
     </div>
   );
 };
