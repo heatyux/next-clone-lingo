b89e0cc162f603f6ca940de3cb53284a854a9800
Lux <1171484646@qq.com>
2025-06-09 15:13:27 +0800

    get user progress query created

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,26 @@
 import { cache } from "react";
 import db from "./drizzle";
-import { coursesTable } from "./schema";
+import { coursesTable, userProgress } from "./schema";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 
 export const getCourses = cache(async () => {
   const data = await db.select().from(coursesTable);
+
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
 
   return data;
 });
