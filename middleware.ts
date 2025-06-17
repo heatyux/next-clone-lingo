a9bb88931a212e149549b8703884d0af747b07a3
Lux <1171484646@qq.com>
2025-06-17 11:31:52 +0800

    stripe added to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // Setting route path
-const isPublicRoute = createRouteMatcher(['/'])
+const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/stripe"]);
 
 export default clerkMiddleware(async (auth, req) => {
   // If route is not matching, check if user is authenticated
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    await auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
