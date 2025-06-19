fc1b42b2f93fa3e9a146069ce44ffd6c4802280f
Lux <1171484646@qq.com>
2025-06-19 16:21:28 +0800

    course list api route completed

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 
 import db from "@/db/drizzle";
+import { getIsAdmin } from "@/lib/admin";
 
 export const GET = async () => {
+  const isAdmin = await getIsAdmin();
+  if (!isAdmin) {
+    return new NextResponse("Unauthorized.", { status: 401 });
+  }
+
   const data = await db.query.coursesTable.findMany();
 
   return NextResponse.json(data);
