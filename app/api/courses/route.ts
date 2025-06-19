2b2a19504c1d47c88fddb6080c0f2139f5115b31
Lux <1171484646@qq.com>
2025-06-19 17:36:52 +0800

    course create route implemented

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,7 +1,8 @@
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 import db from "@/db/drizzle";
 import { getIsAdmin } from "@/lib/admin";
+import { coursesTable } from "@/db/schema";
 
 export const GET = async () => {
   const isAdmin = await getIsAdmin();
@@ -12,4 +13,22 @@ export const GET = async () => {
   const data = await db.query.coursesTable.findMany();
 
   return NextResponse.json(data);
+};
+
+export const POST = async (req: NextRequest) => {
+  const isAdmin = await getIsAdmin();
+  if (!isAdmin) {
+    return new NextResponse("Unauthorized.", { status: 401 });
+  }
+
+  const body = await req.json();
+
+  const data = await db
+    .insert(coursesTable)
+    .values({
+      ...body,
+    })
+    .returning();
+
+  return NextResponse.json(data[0]);
 };
